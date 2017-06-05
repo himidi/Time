1ca54624b957c61b8c13d86590e6747084ac795b
Michael Dilger <midi2001@icloud.com>
2017-06-05 18:40:34 +0200

    Initial Commit

---
+++ Time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time
+//
+//  Created by Michael Dilger on 05.06.17.
+//  Copyright © 2017 Michael Dilger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
