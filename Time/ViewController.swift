b69fa338e32252a5462d1983bea6d404cd3c2762
Michael Dilger <midi2001@icloud.com>
2017-06-24 00:00:41 +0200

    https://github.com/himidi/Time-for-AppleTV.git

--- Time/ViewController.swift
+++ Time/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var timeView: UILabel!
+    let clock = Clock()
+    var timer: Timer?
+        override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.updateTimeLabel), userInfo: nil, repeats: true)
+        
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateTimeLabel()
+    }
+    
+    @objc func updateTimeLabel() {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        timeView.text = formatter.string(from: clock.currentTime as Date)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    deinit {
+        if let timer = self.timer {
+            timer.invalidate()
+        }
+    }
+    
 }
 
